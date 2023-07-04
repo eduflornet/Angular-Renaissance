3535b2a1e6cf6ea58f731ab6d0fe2e8b4134290d
Eduardo Flores <efloresm@minsait.com>
2023-07-04 22:34:36 +0200

    Added FormsModule and implementing ngModel

--- 02-bases/src/app/dbz/dbz.module.ts
+++ 02-bases/src/app/dbz/dbz.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { ListComponent } from './components/list/list.component';
 import { AddCharacterComponent } from './components/add-character/add-character.component';
+
 
 
 
@@ -14,7 +16,7 @@ import { AddCharacterComponent } from './components/add-character/add-character.
   ],
   exports: [MainPageComponent],
   imports: [
-    CommonModule
+    CommonModule, FormsModule
   ]
 })
 export class DbzModule { }
