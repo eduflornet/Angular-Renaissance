b72cc5555fc6d6cf6edfd1d2593a6ada1a870818
Eduardo Flores <efloresm@minsait.com>
2023-07-13 23:19:49 +0200

    fixed app-routing.module, added HomePageComponent

--- spotify/src/app/app-routing.module.ts
+++ spotify/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 
 const routes: Routes = [
   {
-    path:''
+    path: '',//TODO (Private) 🔴🔴
+    component: HomePageComponent,
+    loadChildren: () => import(`./modules/home/home.module`).then(m => m.HomeModule)
     
   }
 ];
