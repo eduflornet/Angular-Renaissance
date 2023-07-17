00e2357acd6186f0233239e40d58b37b96dcfdfe
Eduardo Flores <@eduflornet>
2023-07-17 23:06:50 +0200

    Updated home-page.component html and tracks-routing module

--- spotify/src/app/modules/tracks/tracks-routing.module.ts
+++ spotify/src/app/modules/tracks/tracks-routing.module.ts
@@ -3,6 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { TracksPageComponent } from './pages/tracks-page/tracks-page.component';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: TracksPageComponent,
+  //   outlet: 'child'
+
+  // },
   {
     path: '',
     component: TracksPageComponent
