957c4f3693c09345fa171527fb11fb7e5c76f0c2
Eduardo Flores <@eduflornet>
2023-07-29 23:36:28 +0200

    Added auth.service, updated login-page.component and added enviroments

--- spotify/src/app/modules/auth/pages/login-page/login-page.component.ts
+++ spotify/src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '@modules/auth/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -10,6 +12,10 @@ export class LoginPageComponent implements OnInit {
 
   errorSession: boolean = false
   formLogin: UntypedFormGroup = new UntypedFormGroup({});
+
+
+  constructor(private authService: AuthService,
+    private router: Router) {}
 
   ngOnInit(): void {
     this.formLogin = new UntypedFormGroup(
@@ -30,6 +36,19 @@ export class LoginPageComponent implements OnInit {
 
   sendLogin(): void {
     const { email, password } = this.formLogin.value
+    this.authService.sendCredentials(email, password)
+      //TODO: 200 <400
+      .subscribe(responseOk => { //TODO: Cuando el usuario credenciales Correctas ✔✔
+        console.log('Session iniciada correcta', responseOk);
+        const { tokenSession, data } = responseOk
+        //this.cookie.set('token', tokenSession, 4, '/') //TODO:📌📌📌📌
+        this.router.navigate(['/', 'tracks'])
+      },
+        err => {//TODO error 400>=
+          this.errorSession = true
+          console.log('⚠⚠⚠⚠Ocurrio error con tu email o password');
+        })
+
   }
 
 
---
+++ spotify/src/app/modules/auth/services/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  private readonly URL = environment.api
+
+  sendCredentials(email: string, password: string): Observable<any> {
+    const body = {
+      email,
+      password
+    }
+    return this.http.post(`${this.URL}/auth/login`, body)
+  }
+}
